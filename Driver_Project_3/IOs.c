cddf8200725f0b8b0e089020983d7fd1e4d263cd
Kaleigh Beer <kaleighbeer@gmail.com>
2021-10-31 17:16:50 -0600

    Working code with debounce and serial communication

--- Driver_Project_3/IOs.c
+++ Driver_Project_3/IOs.c
@@ -1,116 +1,173 @@
 #include <xc.h>
 
-#define BUTTON1_PRESSED    (0b00000001)
-#define BUTTON2_PRESSED    (0b00000010)
-#define BUTTON3_PRESSED    (0b00000100)
-#define BUTTON1_2PRESSED   (0b00000011)
-#define BUTTON1_3PRESSED   (0b00000101)
-#define BUTTON2_3PRESSED   (0b00000110)
-#define BUTTON1_2_3PRESSED (0b00000111)
+// Binary values named based on buttons to be used for state machine
 
-uint8_t  CN1flag =  0b00000000;
-uint8_t  CN0flag =  0b00000000;
-uint8_t  CN23flag = 0b00000000;
+#define NO_BUTTON_PRESSED (0b00000000)  // Case of no PB pressed
+#define BUTTON1_PRESSED    (0b00000001) // Case of PB1 pressed corresponds to 1 in bit 0
+#define BUTTON2_PRESSED    (0b00000010) // Case of PB2 pressed corresponds to 1 in bit 1
+#define BUTTON3_PRESSED    (0b00000100) // Case of PB3 pressed corresponds to 1 in bit 2
+#define BUTTON1_2PRESSED   (0b00000011) // Case of PB1 and PB2 pressed corresponds to BUTTON1_PRESSED OR BUTTON_2PRESSED
+#define BUTTON1_3PRESSED   (0b00000101) // Case of PB1 and PB3 pressed corresponds to BUTTON1_PRESSED OR BUTTON_3PRESSED
+#define BUTTON2_3PRESSED   (0b00000110) // Case of PB2 and PB2 pressed corresponds to BUTTON2_PRESSED OR BUTTON_3PRESSED
+#define BUTTON1_2_3PRESSED (0b00000111) // Case of all PBs pressed correspondsto BUTTON1_PRESSED OR BUTTON_2PRESSED OR BUTTON3_PRESSED
 
- uint8_t buttons_pressed = 0b00000000;
+uint8_t  CN1flag =  NO_BUTTON_PRESSED; // Flag indicating that CN interrupt was triggered by PB1
+uint8_t  CN0flag =  NO_BUTTON_PRESSED; // Flag indicating that CN interrupt was triggered by PB2
+uint8_t  CN23flag = NO_BUTTON_PRESSED; // Flag indicating that CN interrupt was triggered by PB3
+
+uint8_t buttons_pressed = NO_BUTTON_PRESSED; // State variable to contain combination of all button presses
+
+int button1 = 0; // Variables used to debounce each PB
+int button2 = 0;
+int button3 = 0;
+
+
+ const int period1 = 500;  // Blink period for PB1 in ms
+ const int period2 = 2000; // Blink period for PB2 in ms
+ const int period3 = 3000; // Blink period for PB3 in ms
+ int period = 0;           // Variable period 
+ 
+ int LED_state = 0;        // Variable will determine if LED stays off or on
+ 
+ const char button_message0[] =     "Nothing pressed          \r"; // Message for no PBs pressed
+ const char button_message1[] =     "PB1 is pressed           \r"; // Message for PB1 pressed
+ const char button_message2[] =     "PB2 is pressed           \r"; // Message for PB2 pressed
+ const char button_message3[] =     "PB3 is pressed           \r"; // Message for PB3 pressed
+ const char button_message1_2[] =   "PB1 and PB2 are pressed  \r"; // Message for PB1 and PB2 pressed
+ const char button_message1_3[] =   "PB1 and PB3 are pressed  \r"; // Message for PB1 and PB3 pressed
+ const char button_message2_3[] =   "PB2 and PB3 are pressed  \r"; // Message for PB2 and PB3 pressed
+ const char button_message1_2_3[] = "All PBs pressed          \r"; // Message for all of PB1, PB2 and PB3 pressed
+ 
+ char* button_message = button_message0; // Initial message is for no PBs pressed
 
 
 void IOinit(); //Function prototype of IOinit()
 void IOcheck(); //Function prototype of IOcheck()
 void Delay_ms(int time_ms); //Function prototype of Delay_ms();
-void blink_LED();
+void blink_LED();   // Function prototype of blink_LED()
 
 
-void blink_LED(int ms) {
+void blink_LED(int ms) { // A function that takes a period (in ms) as an argument and blinks the LED at that period
 
-  LATBbits.LATB8 = 1; // Turn LED on
-  Delay_ms(ms);                 //clear the global flag variable for PB
-  LATBbits.LATB8 = 0;
-  Delay_ms(ms);
+  LATBbits.LATB8 = !LATBbits.LATB8; // Turn LED off or on depending on previous state
+  Delay_ms(ms);                     // Wait for the amount of time specified by ms
+ 
 }
 
 void IOinit(){ //This Function is responsible for initializing the appropriate IO ports
 
-    TRISAbits.TRISA4 = 1; //Sets RA4 as an input (Pushbutton1 input)
-    TRISBbits.TRISB4 = 1; //Sets RB4 as an input (Pushbutton2 input)
-    TRISBbits.TRISB7 = 1; //Sets RB7 as an input (Pushbutton3 input)
-    TRISBbits.TRISB8 = 0; //Set B8 as an output (LED output)
+    TRISAbits.TRISA4 = 1; // Sets RA4 as an input (Pushbutton1 input)
+    TRISBbits.TRISB4 = 1; // Sets RB4 as an input (Pushbutton2 input)
+    TRISBbits.TRISB7 = 1; // Sets RB7 as an input (Pushbutton3 input)
+    TRISBbits.TRISB8 = 0; // Set RB8 as an output (LED output)
     CNPU1bits.CN0PUE = 1; // Pull CN0 - RA4 Up (PU pushbutton1 active low)
     CNPU1bits.CN1PUE = 1; // Pull CN1 - RB4 Up (PU pushbutton2 active low)
     CNPU2bits.CN23PUE = 1; // Pull CN23 - RB7 Up (PU pushbutton3 active low)
 
     CNEN1bits.CN0IE = 1;    //Enable CN0 interrupt
-    CNEN1bits.CN1IE = 1;    //enable CN1 interrupt
-    CNEN2bits.CN23IE = 1;    //Enable CN0 interrupt
-    IPC4bits.CNIP = 4;      //set CN interrupt priority
-    IFS1bits.CNIF = 1;      //clears the CN interrupt flag
-    IEC1bits.CNIE = 1;      //enable the CN interrupt (general)
+    CNEN1bits.CN1IE = 1;    //Enable CN1 interrupt
+    CNEN2bits.CN23IE = 1;   //Enable CN0 interrupt
+    IPC4bits.CNIP = 4;      //Set CN interrupt priority
+    IFS1bits.CNIF = 1;      //Clears the CN interrupt flag
+    IEC1bits.CNIE = 1;      //Enable the CN interrupt (general)
 
 }
 
-void IOcheck(){
-
-  //uint8_t buttons_pressed = 0b00000000;
-
-  int period1 = 500; // Period in ms
-  int period2 = 2000;
-  int period3 = 3000;
-
-
-    switch(buttons_pressed)
+void IOcheck(){                 // This function is responsible for checking which buttons are pressed, controlling the LED and displaying messages
+   
+    switch(buttons_pressed)     // Look at which buttons are pressed
     {
-      case (BUTTON1_PRESSED):
-        blink_LED(period1);
+      case (BUTTON1_PRESSED):   // Case of PB1 being pressed
+          period = period1;     // Period becomes period assigned to PB1
+          button_message = button_message1; // Message becomes message assigned to PB1
         break;
-      case(BUTTON2_PRESSED):
-        blink_LED(period2);
+      case(BUTTON2_PRESSED):    // Case of PB2 being pressed
+          period = period2;     // Period becomes period assigned to PB2
+          button_message = button_message2; // Message becomes message assigned to PB2
         break;
-      case(BUTTON3_PRESSED):
-        blink_LED(period3);
+      case(BUTTON3_PRESSED):    // Case of PB3 being pressed
+          period = period3;     // Period becomes period assigned to PB3
+          button_message = button_message3; // Message becomes message assigned to PB3
         break;
-      case(BUTTON1_2PRESSED):
-      case(BUTTON1_3PRESSED):
-      case(BUTTON2_3PRESSED):
-      case(BUTTON1_2_3PRESSED):
-        LATBbits.LATB8 = 1;
+      case(BUTTON1_2PRESSED):   // Case of PB1 and PB2 being pressed
+          period = 0;           // No blink period; LED will be set to stay on
+          button_message = button_message1_2; // Message becomes message assigned to combination of PB1 and PB2
+          LED_state = 1;        // LED state will be on
+          break;
+      case(BUTTON1_3PRESSED):   // Case of PB1 and PB3 being pressed
+          period = 0;           // No blink period; LED will be set to stay on
+          button_message = button_message1_3; // Message becomes message assigned to combination of PB1 and PB3
+          LED_state = 1;        // LED state will be on
+          break;
+      case(BUTTON2_3PRESSED):   // Case of PB2 and PB3 being pressed
+          period = 0;           // No blink period; LED will be set to stay on
+          button_message = button_message2_3; // Message becomes message assigned to combination of PB2 and PB3
+          LED_state = 1;        // LED state will be on
+          break;
+      case(BUTTON1_2_3PRESSED): // Case of all PBs being pressed
+          period = 0;           // No blink period; LED will be set to stay on
+          button_message = button_message1_2_3; // Message becomes message assigned to combination of all PBs
+          LED_state = 1;        // LED state will be on
         break;
-      default:
-        LATBbits.LATB8 = 0;
+      default:                  // Default case of no buttons being pressed
+          period = 0;           // No blink period; LED will be set to stay off 
+          button_message = button_message0; // Message becomes message displayed for no PBs being pressed
+          LED_state = 0;        // LED state will be off
     }
+    
+        Disp2String(button_message); // Display message in terminal 
+
+        if(!period) {           // If period is zero
+            LATBbits.LATB8 = LED_state; // Set LED to stay on or off depending on assigned state
+         }
+        else {
+            blink_LED(period); // Blink LED at the specified period in ms
+        }
+    
 }
 
-void __attribute__ ((interrupt, no_auto_psv)) _CNInterrupt (void)
+void __attribute__ ((interrupt, no_auto_psv)) _CNInterrupt (void) // ISR triggered by buttons 
 {
-    if(IFS1bits.CNIF == 1)
-    {
-        if (PORTAbits.RA4 == 0){
-            CN0flag = 0b00000001; // user defined global variable used as flag
-        }
-        else{
-            CN0flag = 0;
-        }
-        if(PORTBbits.RB4 ==0)
-        {
-            CN1flag = 0b00000010; // user defined global variable used as flag
+     IFS1bits.CNIF = 0; // clear IF flag
+     
+    button1 = PORTAbits.RA4; // Read state of PB1 
+    button2 = PORTBbits.RB4; // Read state of PB2
+    button3 = PORTBbits.RB7; // Read state of PB3
+    
+    Delay_ms(2); // Debounce - wait for steady state
+    
+    if(PORTAbits.RA4 == button1) { // If current state of PB1 is same as previous state
+        
+        if ((PORTAbits.RA4) == 0){ // If PB1 is pressed
+            CN0flag = BUTTON1_PRESSED; // user defined global variable used as flag
         }
         else {
-            CN1flag = 0;
+            CN0flag = NO_BUTTON_PRESSED;    // If PB1 is not pressed, set flag to 0
         }
-
-        if(PORTBbits.RB7 == 0)
-        {
-            CN23flag = 0b00000100;
-
-        }else {
-            CN23flag = 0;
-        }
-
     }
-   buttons_pressed = CN0flag | CN1flag | CN23flag;
+    if(PORTBbits.RB4 == button2) { // If current state of PB2 is same as previous state
+        
+        if( (PORTBbits.RB4) == 0) // If PB2 is pressed
+        {
+            CN1flag = BUTTON2_PRESSED; // user defined global variable used as flag
+        }
+        else {
+            CN1flag = NO_BUTTON_PRESSED;         // If PB2 is not pressed, set flag to 0
+        }
+    }
+    if(PORTBbits.RB7 == button3) { // If current state of PB3 is same as previous state
+        
+        if( (PORTBbits.RB7) == 0) // If PB3 is pressed
+        {
+            CN23flag = BUTTON3_PRESSED; // user defined global variable used as flag
+        }
+        else {
+            CN23flag = NO_BUTTON_PRESSED;       // If PB3 is not pressed, set flag to 0
+        }
+    }
+   buttons_pressed = CN0flag | CN1flag | CN23flag; // State variable is OR of flags for every button
 
-    IFS1bits.CNIF = 0; // clear IF flag
+    Nop(); // No operation
 
-    Nop();
-
-    return;
+    return; // Return
 }
--- Driver_Project_3/main.c
+++ Driver_Project_3/main.c
@@ -1,3 +1,5 @@
+// 'C' source line config statements
+
 // FBS
 #pragma config BWRP = OFF               // Table Write Protect Boot (Boot segment may be written)
 #pragma config BSS = OFF                // Boot segment Protect (No boot program Flash segment)
@@ -6,26 +8,15 @@
 #pragma config GWRP = OFF               // General Segment Code Flash Write Protection bit (General segment may be written)
 #pragma config GCP = OFF                // General Segment Code Flash Code Protection bit (No protection)
 
-// FOSCSEL
-#pragma config FNOSC = FRCDIV           // Oscillator Select (8 MHz FRC oscillator with divide-by-N (FRCDIV))
-#pragma config IESO = ON                // Internal External Switch Over bit (Internal External Switchover mode enabled (Two-Speed Start-up enabled))
-
-// FOSC
-#pragma config POSCMOD = NONE           // Primary Oscillator Configuration bits (Primary oscillator disabled)
-#pragma config OSCIOFNC = OFF           // CLKO Enable Configuration bit (CLKO output signal is active on the OSCO pin)
-#pragma config POSCFREQ = HS            // Primary Oscillator Frequency Range Configuration bits (Primary oscillator/external clock input frequency greater than 8 MHz)
-#pragma config SOSCSEL = SOSCHP         // SOSC Power Selection Configuration bits (Secondary oscillator configured for high-power operation)
-#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor Selection (Both Clock Switching and Fail-safe Clock Monitor are disabled)
-
 // FWDT
 #pragma config WDTPS = PS32768          // Watchdog Timer Postscale Select bits (1:32,768)
 #pragma config FWPSA = PR128            // WDT Prescaler (WDT prescaler ratio of 1:128)
 #pragma config WINDIS = OFF             // Windowed Watchdog Timer Disable bit (Standard WDT selected; windowed WDT disabled)
-#pragma config FWDTEN = ON              // Watchdog Timer Enable bit (WDT enabled)
+#pragma config FWDTEN = OFF              // Watchdog Timer Enable bit (WDT enabled)
 
 // FPOR
 #pragma config BOREN = BOR3             // Brown-out Reset Enable bits (Brown-out Reset enabled in hardware; SBOREN bit disabled)
-#pragma config PWRTEN = ON              // Power-up Timer Enable bit (PWRT enabled)
+#pragma config PWRTEN = OFF              // Power-up Timer Enable bit (PWRT enabled)
 #pragma config I2C1SEL = PRI            // Alternate I2C1 Pin Mapping bit (Default location for SCL1/SDA1 pins)
 #pragma config BORV = V18               // Brown-out Reset Voltage bits (Brown-out Reset set to lowest voltage (1.8V))
 #pragma config MCLRE = ON               // MCLR Pin Enable bit (MCLR pin enabled; RA5 input pin disabled)
@@ -38,8 +29,16 @@
 #pragma config DSWDTOSC = LPRC          // DSWDT Reference Clock Select bit (DSWDT uses LPRC as reference clock)
 #pragma config RTCOSC = SOSC            // RTCC Reference Clock Select bit (RTCC uses SOSC as reference clock)
 #pragma config DSBOREN = ON             // Deep Sleep Zero-Power BOR Enable bit (Deep Sleep BOR enabled in Deep Sleep)
-#pragma config DSWDTEN = ON             // Deep
+#pragma config DSWDTEN = OFF             // Deep Sleep Watchdog Timer Enable bit (DSWDT enabled)
 
+// CLOCK CONTROL
+#pragma config IESO = OFF    // 2 Speed Startup disabled
+#pragma config FNOSC = FRC  // Start up CLK = 8 MHz
+#pragma config FCKSM = CSECMD // Clock switching is enabled, clock monitor disabled
+#pragma config SOSCSEL = SOSCLP // Secondary oscillator for Low Power Operation
+#pragma config POSCFREQ = MS  //Primary Oscillator/External clk freq betwn 100kHz and 8 MHz. Options: LS, MS, HS
+#pragma config OSCIOFNC = ON  //CLKO output disabled on pin 8, use as IO.
+#pragma config POSCMOD = NONE  // Primary oscillator mode is disabled
 /*
  * The block of code above, from line 1 to line 41, was taken from
  * the example code provided by Professor Parker. I want to give credit
@@ -47,31 +46,32 @@
  * when going over Clock interrupts and delays regarding Driver Project 2
  */
 
-
 #include <xc.h>
 
 void IOinit(); //Function prototype of IOinit()
+void IOcheck(); //Function prototype of IOcheck()
 
 
 int main(void){
     //main() calls the functions IOinit() and IOcheck()
 
-    IOinit();
-    
-    while(1) {
-        IOcheck();
+    IOinit(); // Initialize inputs, outputs and interrupts
+
+    while(1){ // Continuously call IOcheck()
+        IOcheck();  // Check inputs and set outputs
     }
 
     return 0;
 }
 
 
-void __attribute__((interrupt, no_auto_psv)) _T2Interrupt(void)
+void __attribute__((interrupt, no_auto_psv)) _T2Interrupt(void) // ISR for Timer2
 {
     /*
      * Called by the microprocessor automatically when timer2 triggers an interrupt.
      * All this does in this project is clear the timer2 interrupt flag
      */
      IFS0bits.T2IF = 0; //Clears timer2 interrupt flag
+
      return;
 }
